bc97b45a928952fc5b0e6925d982a1770f683cec
Mayank Kumar <mayankkumar.current@gmail.com>
2026-05-12 22:57:20 +0530

    ui and tailwind setup

---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{ts,tsx}", "../../packages/tailwind-config/shared-styles.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
