8a0a6761483631eb7a75bcfea5ddf594f83cc999
Mayank Kumar <mayankkumar.current@gmail.com>
2026-05-13 23:14:13 +0530

    usual fixes and ui started

--- packages/ui/src/components/button.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button({ children }: { children: React.ReactNode }) {
-  return (
-    <button className="bg-foreground">
-      {children}
-    </button>
-  );
-}
